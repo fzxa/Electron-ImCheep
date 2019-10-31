331c48b0efb96c70697ceb38541b97dd02d1ede2
fzxa <wangzhenroot@126.com>
2019-10-31 19:14:41 +0800

    update

--- client/app/main/menu.ts
+++ client/app/main/menu.ts
@@ -12,7 +12,19 @@ const mainMenuTemplate: MenuItemConstructorOptions[]  = [
       { label: 'Paste', accelerator: 'CmdOrCtrl+V', role: 'paste' },
       { label: 'Select All', accelerator: 'CmdOrCtrl+A', role: 'selectall' },
       { label: 'Developers Tools', accelerator: 'Shift+CmdOrCtrl+I /', role: 'Dev Tools' },
-  ]}
+  ]},
+  {
+    label: 'Edit',
+    submenu: [
+      { label: 'Undo', accelerator: 'CmdOrCtrl+Z', role: 'undo' },
+      { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', role: 'redo' },
+      { type: 'separator' },
+      { label: 'Cut', accelerator: 'CmdOrCtrl+X', role: 'cut' },
+      { label: 'Copy', accelerator: 'CmdOrCtrl+C', role: 'copy' },
+      { label: 'Paste', accelerator: 'CmdOrCtrl+V', role: 'paste' },
+      { label: 'Select All', accelerator: 'CmdOrCtrl+A', role: 'selectall' },
+      { label: 'Developers Tools', accelerator: 'Shift+CmdOrCtrl+I /', role: 'Dev Tools' },
+    ]}
 ];
 
 const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
