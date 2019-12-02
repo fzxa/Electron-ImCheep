c0c0eb042262a7a5bc847970e4389710b9b7fd21
fzxa <wangzhenroot@126.com>
2019-12-02 19:27:50 +0800

    update

--- client/app/renderer/components/Organization/index.tsx
+++ client/app/renderer/components/Organization/index.tsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 import './Organization.css';
 
+interface OrganizationProps {
+    get_organization: ()=> void;
+    get_organization_info: ()=>void;
+}
 
-export default class Organization extends Component {
+export default class Organization extends Component<OrganizationProps> {
+
     render() {
+        const { get_organization, get_organization_info } = this.props;
+        console.log(get_organization, get_organization_info);
+        console.log(Organization);
         return (
             <div className="organization">
                 organization,.....
+                123123123123123
+                123123123123123
+                123123123123123
+                123123123123123
+                123123123123123
+                123123123123123
+                123123123123123
             </div>
         );
     }
