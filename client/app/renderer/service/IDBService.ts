8e3036d8209d89e3767c9730f15ff91dd69f4246
fzxa <wangzhenroot@126.com>
2019-12-11 12:57:06 +0800

    feat: add IDBService

--- client/app/renderer/service/IDBService.ts
+++ client/app/renderer/service/IDBService.ts
@@ -14,49 +14,28 @@ export default class IDBService {
         this.indexDB = window.indexedDB;
         this.DB = this.indexDB.open(DB_NAME, DB_VERSION);
 
-        console.log(this.indexDB, this.DB);
-
         this.DB.onsuccess = (e: any)=>{
             this.dbInstance = e.target.result;
-            this.createSchema();
-        }
 
+        }
+        this.createSchema();
     }
 
     public createSchema() {
 
-        // var data:object[] = [{name: "wangzen",age: 24}, {name: "fzxa", age: 18}];
-        console.log('createSchema....');
-        console.log('db instance::::',this.dbInstance);
-        console.log('this DB:::', this.DB);
-        let hasTable:boolean = this.dbInstance.objectStoreNames.contains('test');
-        if(!hasTable) {
-            this.DB.onupgradeneeded = ()=> {
-                console.log("sfsdfsdfsdf")
-                this.dbInstance.createObjectStore('session_users',{
-                    autoIncrement: true,
-                    keyPath: "id"
-                });
-            }
 
-        }
-        // this.DB.onupgradeneeded = () => {
-        //     console.log('sdfdsfdsfsdfdsf..:::::');
-        //     if(!this.dbInstance.objectStoreNames.contains('session_users')) {
-        //         var store:IDBObjectStore = this.dbInstance.createObjectStore("session_users", {
-        //             autoIncrement: true,
-        //             keyPath: "id"
-        //         });
-        //         console.log('store....',store);
-        //         // data.map((value, index)=>{
-        //         //     console.log(value, index)
-        //         //
-        //         // })
-        //     }
-        // }
+        this.DB.onupgradeneeded = (event:any)=>{
+            console.log('onupgradeneeded....')
+            let db:any = event.target.result;
+            let store:IDBObjectStore = db.createObjectStore("session_users", {
+                autoIncrement: true,
+                keyPath: "id"
+            });
+            let data:object[] = [{"name":"zanewang(王振)", "avatar":"/image/avatar.png", "last_message":"请稍后重试账号余额不足充值", "time":"刚刚", "rank": 1, "deleted":0}];
 
-        this.DB.onerror = ()=>{
-            console.log('error....');
+            data.map((value, index)=>{
+                store.add(value)
+            })
         }
 
 
